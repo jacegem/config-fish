f9ffbafba49917157711b33551eb07ba57254c4e
jace <jacegem@gmail.com>
2022-12-04 21:13:40 +0900

    change poetry path

--- config.fish
+++ config.fish
@@ -5,7 +5,8 @@ end
 set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home
 
 set -x PATH /opt/homebrew/bin $PATH
-set -x PATH $HOME/.poetry/bin $PATH
+# set -x PATH $HOME/.poetry/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
 set -x PATH $HOME/SDK/flutter/bin $PATH
 set -x PATH $JAVA_HOME/bin $PATH
 set -x PATH $HOME/.amplify/bin $PATH
