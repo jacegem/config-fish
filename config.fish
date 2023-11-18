9e07924bb9e1a9908d13165e75488841063e9d52
JACE <jacegem@gmail.com>
2023-11-18 11:02:40 +0900

    add mason bin

--- config.fish
+++ config.fish
@@ -7,7 +7,8 @@ set -x HOSTNAME (hostname | string split -m1 '.')[1]
 
 # JAVA_HOME 확인 : /usr/libexec/java_home -V   
 if contains $HOSTNAME mbp23
-    set -x JAVA_HOME /opt/homebrew/Cellar/openjdk/20/libexec/openjdk.jdk/Contents/Home
+    # set -x JAVA_HOME /opt/homebrew/Cellar/openjdk/20/libexec/openjdk.jdk/Contents/Home
+    set -x JAVA_HOME /opt/homebrew/Cellar/openjdk/20.0.1/libexec/openjdk.jdk/Contents/Home
 else
     set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home
 end 
@@ -18,5 +19,6 @@ set -x PATH $HOME/SDK/flutter/bin $PATH
 set -x PATH $JAVA_HOME/bin $PATH
 set -x PATH $HOME/.amplify/bin $PATH
 set -x PATH $HOME/.jenv/bin $PATH
+set -x PATH $HOME/.pub-cache/bin $PATH
 # set -x PATH $HOME/.poetry/bin $PATH
 # set -x VAULT_ADDR 'https://vault.jace.link'
